2d45b9f69a43033f80d85b337fc7586bd32e63ca
quincyocharles <quincyocharles@gmail.com>
2023-06-06 12:08:56 -0700

    a program that prints the ASCII alphabet, in reverse order, alternating lowercase and uppercase (z in lowercase and Y in uppercase) , not followed by a new line

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a') - 1, -1):
     print("{:c}".format(i), end="")
-    print("{:c}".format(i - 32), end="")
+    if i != ord('a'):
+        print("{:c}".format(i - 33), end="")
 
 print()
